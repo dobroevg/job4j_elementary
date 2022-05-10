f9f0dc20c2f5843cf715af167a1ffc765215f23a
dobroevg <eugeny_74@mail.ru>
2022-05-10 13:54:54 +0500

    Загрузка

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class StudentInfo {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
+    public static void main(String[] args) {
+        System.out.println("Dima, privet!");
+        System.out.println("10.02.2022");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+    }
+}
